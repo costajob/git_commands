3ff9a2968eb31b8aec6c23f785371e8ed19e49d2
costajob <costajob@gmail.com>
2016-10-21 10:35:40 +0200

    Mike - fixed broken require

--- lib/git_commands.rb
+++ lib/git_commands.rb
@@ -2,5 +2,5 @@ lib = File.expand_path("../../lib", __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 
 require "git_commands/version"
-require "git_commands/command"
+require "git_commands/computer"
 require "git_commands/cli"
